7c16615bb045613de876249915f615d5f2295f0d
@paulopatto <paulopatto@gmail.com>
2014-04-10 19:44:35 -0300

    Refactory routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 CatarsePagseguro::Engine.routes.draw do
-  namespace :payment do
-    get   '/pagseguro/:id/review'     => 'pagseguro#review',  :as => 'review_pagseguro'
-    post  '/pagseguro/notifications'  => 'pagseguro#ipn',     :as => 'ipn_pagseguro'
-    get   '/pagseguro/:id/pay'        => 'pagseguro#pay',     :as => 'pay_pagseguro'
-    get   '/pagseguro/:id/success'    => 'pagseguro#success', :as => 'success_pagseguro'
-    get   '/pagseguro/:id/cancel'     => 'pagseguro#cancel',  :as => 'cancel_pagseguro'
+  resources :pagseguro, only: [], path: 'payment/pagseguro' do
+    collection do
+      post :ipn # Notification
+    end
+
+    member do
+      get :review
+      get :success
+      get :cancel
+      post :pay
+    end
   end
 end
