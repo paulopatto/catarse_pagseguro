0b28270b0a79606efe1e47550ed5b3160cf4b847
@paulopatto <paulopatto@gmail.com>
2014-04-10 17:56:34 -0300

    changed match -> get on routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ CatarsePagseguro::Engine.routes.draw do
   namespace :payment do
     get   '/pagseguro/:id/review'     => 'pagseguro#review',  :as => 'review_pagseguro'
     post  '/pagseguro/notifications'  => 'pagseguro#ipn',     :as => 'ipn_pagseguro'
-    match '/pagseguro/:id/pay'        => 'pagseguro#pay',     :as => 'pay_pagseguro'
-    match '/pagseguro/:id/success'    => 'pagseguro#success', :as => 'success_pagseguro'
-    match '/pagseguro/:id/cancel'     => 'pagseguro#cancel',  :as => 'cancel_pagseguro'
+    get   '/pagseguro/:id/pay'        => 'pagseguro#pay',     :as => 'pay_pagseguro'
+    get   '/pagseguro/:id/success'    => 'pagseguro#success', :as => 'success_pagseguro'
+    get   '/pagseguro/:id/cancel'     => 'pagseguro#cancel',  :as => 'cancel_pagseguro'
   end
 end
